dc9a03846427de683557a75c75903d86df1c5d48
Dishakatiyar1 <77583219+Dishakatiyar1@users.noreply.github.com>
2023-04-07 16:56:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Home from "./pages/home/Home" ; 
+//import Profile from "./pages/profile/Profile" ;
+//import Register from "./pages/register/Register";
+
+
+function App() {
+  
+   return  < Home />;
+   //return < Profile/>;
+   //return < Register/>;
+  
+}
+  
+export default App;
+    
